6d32f8ea16747d954a8b4f5d04d873bd3900cf30
evan.ward <evan.ward@caci.com>
2025-04-23 15:51:26 -0400

    Pull crafting gear stats

--- src/api.py
+++ src/api.py
@@ -4,31 +4,25 @@ import requests
 class XIVAPI:
     BASE = "https://v2.xivapi.com/api"
 
-    def search(
-        self,
-        # version=one,
-        query,
-        sheets,
-        # cursor,
-        # limit,
-        # schema,
-        fields=None,
-        # transient,
-        language="en",
-    ):
+    def search(self, query, sheets, fields=None, language="en"):
         search_url = self.BASE + "/search"
         fields = ",".join(fields) if fields else None
+        params = {
+            "query": query,
+            "sheets": sheets,
+        }
+        if fields:
+            params["fields"] = fields
         return requests.get(
             search_url,
-            params={
-                "query": query,
-                "sheets": sheets,
-                "fields": fields if fields else [],
-            },
+            params=params,
         ).json()
 
     def search_next(self, next, fields):
-        params = {"cursor": next, "fields": fields}
+        fields = ",".join(fields) if fields else None
+        params = {"cursor": next}
+        if fields:
+            params["fields"] = fields
         search_url = self.BASE + "/search"
         return requests.get(search_url, params=params).json()
 
@@ -37,5 +31,39 @@ class XIVAPI:
         params = {}
         if fields:
             params["fields"] = ",".join(fields)
-        print()
         return requests.get(url, params=params).json()
+
+
+def fetch_item():
+    fields = ["Name", "BaseParam", "BaseParamValue"]
+    client = XIVAPI()
+    query = 'Name~"of Crafting"'
+    gathering_items = []
+    search = client.search(query=query, sheets=["Item"], fields=fields)
+    gathering_items.extend(create_data_dict(search, fields))
+    next = search["next"]
+    while next:
+        search = client.search_next(next=next, fields=fields)
+        next = search["next"] if "next" in search.keys() else None
+        gathering_items.extend(create_data_dict(search, fields))
+    with open("gathering.txt", "w+") as f:
+        f.write(str(gathering_items))
+
+
+def create_data_dict(search, fields):
+    arr = []
+    for entry in search["results"]:
+        data = {
+            "row_id": entry["row_id"],
+        }
+        data.update({field: entry["fields"][field] for field in fields})
+        data["BaseParam"] = [
+            param["fields"]["Name"]
+            for param in data["BaseParam"]
+            if param["fields"]["Name"]
+        ]
+        arr.append(data)
+    return arr
+
+
+fetch_item()
--- src/forge.py
+++ src/forge.py
@@ -1,28 +1,31 @@
-from api import XIVAPI
+import streamlit as st
 
+hq_bonus = 1.2
 
-def fetch_item():
-    client = XIVAPI()
-    query = 'Name~"of Gathering"'
-    gathering_items = []
-    search = client.search(query=query, sheets=["Item"], fields=["Name"])
-    print(search)
-    gathering_items.extend(
-        [
-            {"row_id": entry["row_id"], "name": entry["fields"]["Name"]}
-            for entry in search["results"]
-        ]
-    )
-    next = search["next"]
-    while next:
-        search = client.search_next(next=next, fields=["Name"])
-        next = search["next"] if "next" in search.keys() else None
-        gathering_items.extend(
-            [
-                {"row_id": entry["row_id"], "name": entry["fields"]["Name"]}
-                for entry in search["results"]
-            ]
+stats_col, recipe_col = st.columns([0.3, 0.7])
+with stats_col:
+    stats_container = st.container(border=True)
+    cms = stats_container.text_input("Craftsmanship")
+    ctl = stats_container.text_input("Control")
+    cp = stats_container.text_input("CP")
+
+with recipe_col:
+    recipe_container = st.container(border=True)
+    recipe_col1, recipe_col2 = recipe_container.columns([0.1, 0.9])
+    with recipe_col1:
+        recipe_image = st.image(
+            "https://i.redd.it/n4ekwnitpdsa1.jpg", use_container_width=True
         )
-    with open("gathering.txt", "w+") as f:
-        f.write(str(gathering_items))
-    print(len(gathering_items))
+    with recipe_col2:
+        recipe_name = st.selectbox("Choose a recipe", ("r1", "r2"))
+    progress_container = st.container(border=True)
+    progress = progress_container.progress(value=0, text="Progress")
+    quality = progress_container.progress(value=0, text="Quality")
+actions_container = st.container(border=True)
+current_sim = actions_container.container(border=True, height=500)
+action_list = actions_container.container(border=True)
+action_col1, action_col2, action_col3, action_col4 = action_list.columns(4)
+with action_col1:
+    crop = st.image("https://i.redd.it/n4ekwnitpdsa1.jpg", use_container_width=True)
+with action_col2:
+    crop2 = st.image("https://i.redd.it/n4ekwnitpdsa1.jpg", use_container_width=True)
